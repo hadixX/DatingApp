06f681730415ed1a68f5fa1b9e6f56559a0188f6
hadi <hadigharaibeh.95@gmail.com>
2021-03-01 01:19:05 +0200

    add pagelist

---
+++ DatingApp.API/handlers/PaginationHeader.cs
@@ -0,0 +1,19 @@
+namespace DatingApp.API.handlers
+{
+    public class PaginationHeader
+    {
+        public PaginationHeader(int currentPage, int itemPerPage, int totalItem, int totalPage)
+        {
+            this.CurrentPage = currentPage;
+            this.ItemPerPage = itemPerPage;
+            this.TotalItem = totalItem;
+            this.TotalPage = totalPage;
+
+        }
+        public int CurrentPage { get; set; }
+        public int ItemPerPage { get; set; }
+        public int TotalItem { get; set; }
+        public int TotalPage { get; set; }
+
+    }
+}
